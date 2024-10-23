68028704ed768a9a500adbf6bf390722f46dc583
fukuda <sfu983667@gmail.com>
2024-10-23 11:55:53 +0900

    認証メールのリンクルート

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill(); // メール確認を完了
+
+    return redirect('http://localhost:3000/login');
+
+})->middleware(['auth', 'signed'])->name('verification.verify');

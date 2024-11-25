4e054b8c4d5af95cbfd27589d10242ba34cbb97c
fukuda983667 <sfu983667@gmail.com>
2024-11-25 23:53:38 +0900

    全商品取得と商品詳細取得

---
+++ src/app/Http/Controllers/ItemController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Item;
+use App\Models\Like;
+use App\Models\Comment;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Config;
+
+class ItemController extends Controller
+{
+    // 全item取得
+    public function getItems()
+    {
+        $userId = Auth::id(); // ログイン中のユーザーIDを取得
+        $baseUrl = Config::get('app.url') . '/storage/items/'; //envのAPP_URLを利用して商品画像を保存しているディレクトリのパスを設定
+
+        // ログイン中のユーザーが出品した商品を除外、ゲストユーザ($userIdがnull)はそのまま全商品をフロントに渡す。
+        $query = Item::query();
+        if (!is_null($userId)) {
+            $query->where('user_id', '!=', $userId);
+        }
+
+        // アイテムを取得し、image_path プロパティを追加
+        $items = $query->get()->map(function ($item) use ($baseUrl) {
+            $item->image_path = $item->image_path ? $baseUrl . $item->image_path : null;
+            return $item;
+        });
+
+        // 取得したアイテムを JSON 形式で返す
+        return response()->json(compact('items'), 200);
+    }
+
+
+    // 指定のitemの詳細を取得
+    public function getItem($id)
+    {
+        $userId = Auth::id(); // ログイン中のユーザーIDを取得
+        $baseUrl = Config::get('app.url') . '/storage/items/';
+
+        // 指定された ID のアイテムを取得し、関連するカテゴリも取得
+        $item = Item::with('categories')->find($id);
+
+        // アイテムが存在しない場合は 404 エラーを返す
+        if (!$item) {
+            return response()->json(['error' => 'Item not found'], 404);
+        }
+
+        // 画像パスを構築
+        $item->image_path = $item->image_path ? $baseUrl . $item->image_path : null;
+
+        // condition のテキストを追加 1~4で管理してるやつ
+        $item->condition_text = $item->condition_text;
+
+
+        // アイテム詳細を JSON 形式で返す
+        return response()->json(compact('item'), 200);
+    }
+}

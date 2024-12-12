b75c48059e3681c8931630810fe97f39f138d276
fukuda983667 <sfu983667@gmail.com>
2024-12-13 00:03:42 +0900

    お気に入り機能テスト

---
+++ src/tests/Feature/LikeTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Http;
+use Tests\TestCase;
+
+
+class LikeTest extends TestCase
+{
+    use RefreshDatabase;
+
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // テスト開始前にマイグレーションをリフレッシュし、シーディングを実行 少し時間がかかる
+        Artisan::call('migrate:refresh --seed --env=testing');
+    }
+
+
+    // ログインユーザは商品をお気に入り登録できる
+
+}

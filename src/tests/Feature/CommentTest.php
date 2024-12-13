862bb701056d9f4824f569c0448f0ec77690ae52
fukuda983667 <sfu983667@gmail.com>
2024-12-14 00:18:55 +0900

    コメント機能テスト

---
+++ src/tests/Feature/CommentTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+
+
+class CommentTest extends TestCase
+{
+    use RefreshDatabase;
+
+
+}

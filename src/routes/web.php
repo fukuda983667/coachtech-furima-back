defbb0b721b644533b8fe8a0df9cf851d16039d8
fukuda983667 <sfu983667@gmail.com>
2024-12-21 17:06:47 +0900

    コメント追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -19,14 +19,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+// テスト用
 Route::get('web/helloworld', function () {
     return response()->json(['message' => 'Hello, World [web]']);
 });
 
 
+// 認証メールのリンクとび先
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill(); // メール確認を完了
 
+    // フロントエンドのプロフィール編集画面にリダイレクト
     return redirect('http://localhost:3000/mypage/profile');
 
 })->middleware(['auth', 'signed'])->name('verification.verify');
